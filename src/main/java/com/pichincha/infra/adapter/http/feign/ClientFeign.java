c87b677a0e249b176bad6adc0189c2102c9ffd11
Juan Luis Aguilar Garcia <juanaguilargarcia20@gmail.com>
2025-03-03 15:08:25 -0500

    ajuste puego d ela s pruebas

--- src/main/java/com/pichincha/infra/adapter/http/feign/ClientFeign.java
+++ src/main/java/com/pichincha/infra/adapter/http/feign/ClientFeign.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "clients", url = "${spring.cloud.openfeign.client.config.clients.url}",  fallback = ClientFeignFallback.class)
+@FeignClient(name = "clients", url = "${spring.cloud.openfeign.client.config.clients.url}" )
 public interface ClientFeign {
     @GetMapping(value = "/{client_id}", produces = "application/json")
     ClientDto getClientById(@PathVariable("client_id") Long clientId) throws ClientFeingException;
